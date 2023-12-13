59e3322180304402e216344a4940d77ce6aca543
bnasare <124492150+bnasare@users.noreply.github.com>
2023-12-13 21:39:40 +0000

    user registration

---
+++ src/firebase/config.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAS1EDatep8y4dhOUVU6QNVxehSdtTt_cs",
+    authDomain: "time-tracker-app-43b0c.firebaseapp.com",
+    projectId: "time-tracker-app-43b0c",
+    storageBucket: "time-tracker-app-43b0c.appspot.com",
+    messagingSenderId: "128873006707",
+    appId: "1:128873006707:web:8701fd49ff634ca4c70c06"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,33 @@
 import React, { useEffect, useState } from "react";
+import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth'
+import { useNavigate } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
+import app from "../firebase/config";
+
+//Auth instance
+const auth = getAuth(app);
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const [loginLoading, setLoginLoading] = useState(false);
+  const [registerLoading, setRegisterLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  async function signUp() {
+    setRegisterLoading(true);
+    setError(null);
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      alert("Account created successfully!");
+      navigate("/reports");
+    }
+    catch (error) {
+      setError(error.message);
+    }
+  }
+
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
@@ -25,6 +52,7 @@ const Login = () => {
                   name="email"
                   type="email"
                   autoComplete="email"
+                  onChange={(e) => setEmail(e.target.value)}
                   required
                   className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                 />
@@ -44,6 +72,7 @@ const Login = () => {
                   name="password"
                   type="password"
                   autoComplete="current-password"
+                  onChange={(e) => setPassword(e.target.value)}
                   required
                   className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                 />
@@ -55,6 +84,7 @@ const Login = () => {
               <button
                 type="button"
                 className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                onClick={signUp}
               >
                 Register
               </button>

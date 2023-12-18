92f07947f68fd764c17f870fb6c69ad7f96bccfa
bnasare <124492150+bnasare@users.noreply.github.com>
2023-12-18 08:51:21 +0000

    updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 const navigation = [
   {
     name: "YouTube",
-    href: "https://www.youtube.com/channel/UCqgi3TTpWwO22hIxzPOLhWw",
+    href: "https://www.youtube.com/@JeronDev",
     icon: (props) => (
       <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
         <path
@@ -12,22 +12,22 @@ const navigation = [
       </svg>
     ),
   },
-  {
-    name: "Facebook",
-    href: "https://www.youtube.com/channel/UCqgi3TTpWwO22hIxzPOLhWw",
-    icon: (props) => (
-      <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-        <path
-          fillRule="evenodd"
-          d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
-          clipRule="evenodd"
-        />
-      </svg>
-    ),
-  },
+  // {
+  //   name: "Facebook",
+  //   href: "https://www.youtube.com/channel/UCqgi3TTpWwO22hIxzPOLhWw",
+  //   icon: (props) => (
+  //     <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+  //       <path
+  //         fillRule="evenodd"
+  //         d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
+  //         clipRule="evenodd"
+  //       />
+  //     </svg>
+  //   ),
+  // },
   {
     name: "Instagram",
-    href: "https://www.youtube.com/channel/UCqgi3TTpWwO22hIxzPOLhWw",
+    href: "https://www.instagram.com/bank.himself",
     icon: (props) => (
       <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
         <path
@@ -39,8 +39,8 @@ const navigation = [
     ),
   },
   {
-    name: "https://www.youtube.com/channel/UCqgi3TTpWwO22hIxzPOLhWw",
-    href: "#",
+    name: "Twitter",
+    href: "https://twitter.com/_bornContrarian",
     icon: (props) => (
       <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
         <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
@@ -49,7 +49,7 @@ const navigation = [
   },
   {
     name: "GitHub",
-    href: "#",
+    href: "https://github.com/bnasare",
     icon: (props) => (
       <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
         <path
@@ -80,7 +80,7 @@ export default function Footer() {
         </div>
         <div className="mt-8 md:order-1 md:mt-0">
           <p className="text-xs leading-5 text-center text-gray-500">
-            &copy; 2023 Inovotek Coding Academy, Inc. All rights reserved.
+            &copy; {new Date().getFullYear()} Tevrozo Inc. All rights reserved.
           </p>
         </div>
       </div>
